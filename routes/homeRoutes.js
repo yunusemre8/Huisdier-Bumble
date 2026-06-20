158e454884ad2f918370f63dac1a516b1abbf0d1
cero-c <ceren.coban@hva.nl>
2026-06-20 11:29:09 +0200

    homeRoutes-login-check-added

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', home)
-
-function home(req, res) {
+router.get('/', (req,res) => {
+  if(req.session.userId) {
+    return res.redirect(`/profile/${req.session.userId}`)
+  }
   res.render('home')
-}
+})
 
 module.exports = router
